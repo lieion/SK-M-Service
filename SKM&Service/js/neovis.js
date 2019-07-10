d913e68ad9b7af2ff695ce80a319bbb8321b5957
LeeSeungYeol <50110372+LeeSeungYeol@users.noreply.github.com>
2019-07-10 15:45:38 +0900

    Add files via upload

--- SKM&Service/js/neovis.js
+++ SKM&Service/js/neovis.js
@@ -36685,6 +36685,43 @@ class NeoVis {
                 //          return item;
                 //     }
                 // );
+                var test = document.getElementById("context-menus");
+
+                /* 마우스 클릭 리스너를 초기 실행시킨다. */
+                function init() {
+                  rightMouseListener();
+                  leftMouseListener();
+                }
+
+                /* 마우스 왼클릭 감지 */
+                function leftMouseListener() {
+                  document.addEventListener("click", function(e) {
+                    toggleOnOff(0);
+                  })
+                }
+
+                /* 마우스 우클릭 감지 */
+                function rightMouseListener() {
+                  document.addEventListener("contextmenu", function(e) {
+                    event.preventDefault();
+                    toggleOnOff(1);
+                    showMenu(e.x, e.y);
+                  });
+                }
+
+                /* 마우스 메뉴 on & off */
+                function toggleOnOff(num) {
+                  num === 1 ? test.classList.add("active") : test.classList.remove("active");
+                }
+
+                /* 마우스 클릭한 지점에서 메뉴 보여줌 */
+                function showMenu(x, y) {
+                  console.log(test);
+                  test.style.top = y + "px";
+                  test.style.left = x + "px";
+
+                }
+
                 
                 self._network = new __WEBPACK_IMPORTED_MODULE_1__vendor_vis_dist_vis_network_min_js__["Network"](container, self._data, options);
                 self._network.on("selectNode", function (params) {
@@ -36711,7 +36748,7 @@ class NeoVis {
                     }
                     guessing=0;
 
-                    
+                    init();
                 }); 
 
                 //console.log(self._events);
